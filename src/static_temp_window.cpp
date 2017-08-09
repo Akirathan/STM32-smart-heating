9d2406c3498f824982f13f76b113af129c67928d
mayfa <pavel.akira.marek@gmail.com>
2017-08-09 14:46:11 +0200

    Add StaticPresetTempWindow.

--- src/static_temp_window.cpp
+++ src/static_temp_window.cpp
@@ -94,3 +94,42 @@ void StaticMeasureTempWindow::registerMinCallback()
 {
 	RTCController::getInstance().registerMinuteCallback(this);
 }
+
+StaticPresetTempWindow::StaticPresetTempWindow(const Coord& c)
+	: StaticTempWindow(c)
+{ }
+
+StaticPresetTempWindow::~StaticPresetTempWindow()
+{
+	if (registeredCallback) {
+		RTCController::getInstance().unregisterMinuteCallback(this);
+	}
+}
+
+void StaticPresetTempWindow::minCallback()
+{
+	// Get supposed temperature from TempController and redraw this window.
+	TempController& tempController = TempController::getInstance();
+	temp = (double)tempController.currentIntervalTemperature();
+	draw();
+}
+
+void StaticPresetTempWindow::registerMinCallback()
+{
+	RTCController::getInstance().registerMinuteCallback(this);
+}
+
+/**
+ * Registers for minute callback.
+ */
+void StaticPresetTempWindow::showPresetTemp()
+{
+	RTCController& rtc = RTCController::getInstance();
+
+	if (!rtc.isTimeSet()) {
+		//TODO: error handling
+	}
+
+	registerMinCallback();
+	registeredCallback = true;
+}
