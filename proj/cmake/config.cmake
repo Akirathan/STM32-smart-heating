8b4c1a7a1088438d8f66e99962c72b573d36817e
mayfa <pavel.akira.marek@gmail.com>
2018-02-22 16:51:01 +0100

    Add cmake pre-script

---
+++ proj/cmake/config.cmake
@@ -0,0 +1,28 @@
+
+set(ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../..")
+set(BUILD_DIR "${ROOT_DIR}/build")
+set(TOOLCHAIN_FILE "toolchain.cmake")
+
+# Set these variables according to your system.
+set(CMAKE "cmake")
+set(VERBOSE_MAKEFILE TRUE)
+
+
+if (VERBOSE_MAKEFILE)
+  set(VERBOSE_MAKEFILE_OPT "-DCMAKE_VERBOSE_MAKEFILE=TRUE")
+else()
+  set(VERBOSE_MAKEFILE_OPT "")
+endif()
+
+execute_process(COMMAND
+  "${CMAKE}"
+  "-DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}"
+  "${VERBOSE_MAKEFILE_OPT}"
+  "-B${BUILD_DIR}"
+  "-H."
+)
+
+# Create build script
+file(WRITE "build.bat"
+  "${CMAKE} --build ${BUILD_DIR}"
+)
