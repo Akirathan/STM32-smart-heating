cd708f85165bdb34f7c76d0eb731c9dca33598cf
Unknown <pavel.akira.marek@gmail.com>
2018-02-24 18:33:10 +0100

    Enable SWD

--- src/cube/cube-mx/Src/main.c
+++ src/cube/cube-mx/Src/main.c
@@ -264,6 +264,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(TEMP_DATA_GPIO_Port, TEMP_DATA_Pin, GPIO_PIN_RESET);
