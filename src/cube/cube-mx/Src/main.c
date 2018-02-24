f388c195f06b404d3b875baa583a876b2588e0dc
Unknown <pavel.akira.marek@gmail.com>
2018-02-24 18:26:45 +0100

    Remove infinite loop from cube_main

--- src/cube/cube-mx/Src/main.c
+++ src/cube/cube-mx/Src/main.c
@@ -111,14 +111,11 @@ int cube_main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
 
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
 
-  }
   /* USER CODE END 3 */
 
 }
