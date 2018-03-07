6c4fad3018bff27b7cbcd145bbe2c10b5615a1fb
mayfa <pavel.akira.marek@gmail.com>
2018-03-07 21:53:51 +0100

    Add USE_CUBE_MX variable to cmake
    Usage of cube-mx is optional

--- proj/cmake/config.cmake
+++ proj/cmake/config.cmake
@@ -3,11 +3,17 @@ set(ROOT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../..")
 set(BUILD_DIR "${ROOT_DIR}/build")
 set(TOOLCHAIN_FILE "toolchain.cmake")
 
+###########            CONFIG    ####################################
 # Set these variables according to your system.
 set(CMAKE "cmake")
 set(VERBOSE_MAKEFILE FALSE)
 set(GENERATOR "Unix Makefiles")
 
+# If true, sources from cube-mx will be linked, otherwise sources from
+# hardware directory will be linked.
+set(USE_CUBE_MX FALSE)
+
+######################################################################
 
 if (VERBOSE_MAKEFILE)
   set(VERBOSE_MAKEFILE_OPT "-DCMAKE_VERBOSE_MAKEFILE=TRUE")
@@ -19,6 +25,7 @@ execute_process(COMMAND
   "${CMAKE}"
   "-G${GENERATOR}"
   "-DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}"
+  "-DUSE_CUBE_MX=${USE_CUBE_MX}"
   "${VERBOSE_MAKEFILE_OPT}"
   "-B${BUILD_DIR}"
   "-H."
