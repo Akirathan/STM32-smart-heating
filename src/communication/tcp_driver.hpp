92a2d8bb8b99a23fd60b548467cff35a96cea461
mayfa <pavel.akira.marek@gmail.com>
2018-06-22 15:20:55 +0200

    Add TcpDriver

---
+++ src/communication/tcp_driver.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file tcp_driver.hpp
+ * @author mayfa
+ * @date Jun 22, 2018
+ */
+
+#ifndef TCP_DRIVER_HPP_
+#define TCP_DRIVER_HPP_
+
+
+class TcpDriver {
+public:
+	static void init();
+};
+
+
+#endif /* TCP_DRIVER_HPP_ */
