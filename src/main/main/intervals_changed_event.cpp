43963bf9bb6088d9a7cc6ddc4e65080f9a08d4aa
mayfa <pavel.akira.marek@gmail.com>
2018-06-24 15:07:11 +0200

    Implement IntervalsChangedEvent as immutable

---
+++ src/main/main/intervals_changed_event.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file intervals_changed_event.cpp
+ * @author mayfa
+ * @date Jun 24, 2018
+ */
+
+#include "intervals_changed_event.hpp"
+#include "rt_assert.h"
+
+
+IntervalsChangedEvent::IntervalsChangedEvent(IntervalFrameData data[], const size_t count)
+{
+	rt_assert(count <= INTERVALS_NUM, "Too much intervals");
+
+	for (size_t i = 0; i < count; i++) {
+		this->data[i] = data[i];
+	}
+	this->count = count;
+}
+
+const IntervalFrameData * IntervalsChangedEvent::getData(size_t *count)
+{
+	rt_assert(count != nullptr, "Unexpected null pointer");
+
+	*count = this->count;
+	return data;
+}
--- src/main/main/intervals_changed_event.hpp
+++ src/main/main/intervals_changed_event.hpp
@@ -7,17 +7,24 @@
 #ifndef INTERVALS_CHANGED_EVENT_HPP_
 #define INTERVALS_CHANGED_EVENT_HPP_
 
+#include <cstddef> // For size_t
 #include "event.hpp"
 #include "interval_frame_data.hpp"
+#include "settings.h" // For INTERVALS_NUM
 
 /**
  * Represents intervals changed event.
+ * Event arguments are immutable.
  *
  * See @ref Application::emitEvent for more details.
  */
 class IntervalsChangedEvent : public Event {
 public:
-	IntervalsChangedEvent(IntervalFrameData data[], size_t count);
+	IntervalsChangedEvent(IntervalFrameData data[], const size_t count);
+	const IntervalFrameData * getData(size_t *count);
+private:
+	IntervalFrameData data[INTERVALS_NUM];
+	size_t count;
 };
 
 
