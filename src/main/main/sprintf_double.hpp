6e2089d94d43f9cae7e90773ec772752602eb1b3
mayfa <pavel.akira.marek@gmail.com>
2018-06-27 18:08:20 +0200

    Add sprintf_double.hpp

---
+++ src/main/main/sprintf_double.hpp
@@ -0,0 +1,30 @@
+/**
+ * @file sprintf_double.hpp
+ * @author Pavel Marek
+ * @date Jun 27, 2018
+ */
+
+#ifndef SPRINTF_DOUBLE_HPP_
+#define SPRINTF_DOUBLE_HPP_
+
+#include <cstdio> // For std::sprintf
+
+/**
+ * Sprintf for double with fixed 4 precision.
+ * Note that this is necessary because there is no support for printing floats
+ * in nano lib.
+ * @param str ... this string will be filled with @p d converted to string.
+ * @param d
+ */
+inline int sprintf_double(char str[], double d)
+{
+	char *sign = (d < 0) ? "-" : "";
+	d = (d < 0) ? -d : d;
+
+	int int_part = static_cast<int>(d);
+	double frac_part = d - int_part;
+	int frac_part_mult = frac_part * 10000;
+	return std::sprintf(str, "%s%d.%04d", sign, int_part, frac_part_mult);
+}
+
+#endif /* SPRINTF_DOUBLE_HPP_ */
