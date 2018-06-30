3a11f5a4c8e587a924f2315a6b7c2b4fb70b5717
mayfa <pavel.akira.marek@gmail.com>
2018-06-30 20:04:53 +0200

    Split IntervalsChangedEvnet also into IntervalsChangedServerEvent

--- src/main/main/intervals_changed_event.cpp
+++ src/main/main/intervals_changed_event.cpp
@@ -15,6 +15,11 @@ const IntervalFrameData * IntervalsChangedEvent::getData(size_t *count) const
 	return data;
 }
 
+const IntervalList & IntervalsChangedEvent::getList() const
+{
+	return intervalList;
+}
+
 uint32_t IntervalsChangedEvent::getTimestamp() const
 {
 	return timestamp;
@@ -37,7 +42,40 @@ IntervalsChangedEvent::IntervalsChangedEvent(IntervalFrameData data[], const siz
 {
 	rt_assert(count <= INTERVALS_NUM, "Too much intervals");
 
+	// Initialize data.
 	for (size_t i = 0; i < count; i++) {
 		this->data[i] = data[i];
+	}
+
+	// Initialize intervalList.
+	convertDataToList(&intervalList, data, dataCount);
+	intervalList.setTimestamp(timestamp);
+}
+
+IntervalsChangedEvent::IntervalsChangedEvent(const IntervalList &interval_list, bool timestamp_synced) :
+	dataCount(interval_list.getIntervalsCount()),
+	intervalList(interval_list),
+	timestamp(interval_list.getTimestamp()),
+	timestampSynced(timestamp_synced)
+{
+	rt_assert(interval_list.getIntervalsCount() <= INTERVALS_NUM, "Too much intervals");
+	rt_assert(interval_list.getTimestamp() != 0, "Timestamp must be set");
+
+	// Initialize data.
+	for (size_t i = 0; i < interval_list.getIntervalsCount(); i++) {
+		data[i].from = interval_list.getInterval(i)->getFromTime();
+		data[i].to = interval_list.getInterval(i)->getToTime();
+		data[i].temp = interval_list.getInterval(i)->getTemp();
+	}
+}
+
+void IntervalsChangedEvent::convertDataToList(IntervalList *interval_list,
+		const IntervalFrameData data[], size_t data_count)
+{
+	rt_assert(interval_list != nullptr, "Unexpected nullptr");
+
+	for (size_t i = 0; i < data_count; i++) {
+		Interval interval(data[i].from, data[i].to, data[i].temp);
+		interval_list->addInterval(interval);
 	}
 }
--- src/main/main/intervals_changed_event.hpp
+++ src/main/main/intervals_changed_event.hpp
@@ -25,17 +25,21 @@
 class IntervalsChangedEvent : public Event {
 public:
 	const IntervalFrameData * getData(size_t *count) const;
+	const IntervalList & getList() const;
 	uint32_t getTimestamp() const;
 	bool isTimestampSynced() const;
 protected:
 	IntervalsChangedEvent(IntervalFrameData data[], const size_t count, const uint32_t timestamp,
 	         	          bool timestamp_synced);
+	IntervalsChangedEvent(const IntervalList &interval_list, bool timestamp_synced);
 private:
 	IntervalFrameData data[INTERVALS_NUM];
 	size_t dataCount;
 	IntervalList intervalList;
 	uint32_t timestamp;
 	bool timestampSynced;
+
+	void convertDataToList(IntervalList *interval_list, const IntervalFrameData data[], size_t data_count);
 };
 
 
