149d57770e4103bbd62444d4620d4ae7fe82c08b
mayfa <pavel.akira.marek@gmail.com>
2018-06-30 15:23:17 +0200

    Implement trivial CharStreamTest

--- src/tests/char_stream_test.cpp
+++ src/tests/char_stream_test.cpp
@@ -5,13 +5,19 @@
  */
 
 #include "char_stream_test.hpp"
+#include "char_stream.hpp"
 
 void CharStreamTest::runAll()
 {
+	char *buffer = "Content-Length: 0\r\n\r\n";
+	char words[2][20];
+	size_t word_count = 0;
 
+	CharStream char_stream(buffer, std::strlen(buffer));
+	char_stream.readLine(words, &word_count, ':');
 }
 
 bool CharStreamTest::passed()
 {
-
+	return false;
 }
