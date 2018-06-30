a1c94328f1ee684573bd1c43ad4915cf7196ade8
mayfa <pavel.akira.marek@gmail.com>
2018-06-30 15:14:58 +0200

    Add very simple tests, link them to main binary

---
+++ src/tests/char_stream_test.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file char_stream_test.cpp
+ * @author mayfa
+ * @date Jun 30, 2018
+ */
+
+#include "char_stream_test.hpp"
+
+void CharStreamTest::runAll()
+{
+
+}
+
+bool CharStreamTest::passed()
+{
+
+}
