03a98fb02de243216feb5bff6ad7d6164f12f4d8
mayfa <pavel.akira.marek@gmail.com>
2018-07-06 15:21:24 +0200

    Fix KeyFrame::KeyFrame - initialization of all hexCharWindows and correct X coordinates

--- src/main/gui/frames/key_frame.cpp
+++ src/main/gui/frames/key_frame.cpp
@@ -9,21 +9,35 @@
 
 KeyFrame::KeyFrame()
 {
+	sFONT *font = LCD::get_font();
 	uint32_t x_size = LCD::get_x_size();
-	uint32_t first_row_y = LINE(3);
-	uint16_t x_0 = x_size/8 - 5;
-	uint16_t x_1 = 2 * (x_size/8) - 5;
-	// ...
-	uint16_t submit_button_x = x_size/2 - 20;
-	uint16_t submit_button_y = LINE(6);
 
-	hexCharWindows[0] = HexCharWindow(Coord(x_0, first_row_y));
-	hexCharWindows[1] = HexCharWindow(Coord(x_1, first_row_y));
+	// Count all coordinates.
+	uint32_t first_row_y = LINE(4);
+	uint32_t second_row_y = LINE(6);
 
+	uint16_t x[HEX_CHAR_WINDOWS_SIZE/2];
+	x[0] = x_size/8 - 5;
+	for (size_t i = 1; i < HEX_CHAR_WINDOWS_SIZE/2; i++) {
+		x[i] = x[i-1] + font->Width * 2;
+	}
+
+	uint16_t submit_button_x = x_size/2 - 30;
+	uint16_t submit_button_y = LINE(8);
+
+	// Create all window objects.
+	for (size_t i = 0; i < HEX_CHAR_WINDOWS_SIZE/2; i++) {
+		hexCharWindows[i] = HexCharWindow(Coord(x[i], first_row_y));
+	}
+	for (size_t i = HEX_CHAR_WINDOWS_SIZE/2; i < HEX_CHAR_WINDOWS_SIZE; i++) {
+		hexCharWindows[i] = HexCharWindow(Coord(x[i % (HEX_CHAR_WINDOWS_SIZE/2)], second_row_y));
+	}
 	submitButton = Button(Coord(submit_button_x, submit_button_y), "SUBMIT");
 
-	windowSystem.addControl(&hexCharWindows[0]);
-	windowSystem.addControl(&hexCharWindows[1]);
+	// Add all window objects to windowSystem.
+	for (size_t i = 0; i < HEX_CHAR_WINDOWS_SIZE; i++) {
+		windowSystem.addControl(&hexCharWindows[i]);
+	}
 	windowSystem.addControl(&submitButton);
 }
 
@@ -47,7 +61,7 @@ void KeyFrame::registerExitMessageCallback()
 
 void KeyFrame::drawHeader()
 {
-	const uint8_t *header = reinterpret_cast<const uint8_t *>("Insert key from server");
+	const uint8_t *header = reinterpret_cast<const uint8_t *>("Insert key");
 	LCD::draw_header(const_cast<uint8_t *>(header));
 }
 
